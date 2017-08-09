987705b90a1985fef0024e39093c6dada88121ea
Kenneth Sorling <kennethsorling@mail.com>
2017-08-09 05:26:10 +0200

    Updated seed method and tidied up the Teachers Index.

--- LexiconLMS/Migrations/Configuration.cs
+++ LexiconLMS/Migrations/Configuration.cs
@@ -19,10 +19,98 @@ namespace LexiconLMS.Migrations
         {
             context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "E-Learning" });
             context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "Lecture" });
-            context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "Assignment" });
             context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "Code-Along" });
-            context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "APL" });
             context.ActivityTypes.AddOrUpdate(a => a.TypeName, new ActivityType { TypeName = "Project" });
+
+            string loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course {
+                Name = ".Net Developer",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Java Developer",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Excel",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "PowerPoint",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Outlook",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "OneNote",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Access",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Web och App",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "SharePoint",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Grafisk Produktion",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
+
+            context.Courses.AddOrUpdate(c => c.Name, new Course
+            {
+                Name = "Crystal Reports",
+                Description = loremipsum,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(4)
+            });
 
             var userStore = new UserStore<ApplicationUser>(context);
             var userManager = new UserManager<ApplicationUser>(userStore);
@@ -52,6 +140,26 @@ namespace LexiconLMS.Migrations
                 };
                 result = userManager.Create(user, "VerySecret123!");
                 result = userManager.AddToRole(user.Id, "Teacher");
+
+                user = new Teacher
+                {
+                    FirstName = "John",
+                    LastName = "Hellman",
+                    Email = "john.hellman@lexicon.se",
+                    UserName = "john.hellman@lexicon.se"
+                };
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Teacher");
+
+                user = new Teacher
+                {
+                    FirstName = "Dmitris",
+                    LastName = "Björlingh",
+                    Email = "dmitris.bjorlingh@lexicon.se",
+                    UserName = "dmitris.bjorlingh@lexicon.se"
+                };
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Teacher");
             }
 
             context.Courses.AddOrUpdate(c => c.Name, new Course { Name = "Java for dummies", StartDate = System.DateTime.Today, EndDate = System.DateTime.Today, Description = "Booga" });
@@ -69,6 +177,57 @@ namespace LexiconLMS.Migrations
                 };
                 result = userManager.Create(user, "VerySecret123!");
                 result = userManager.AddToRole(user.Id, "Student");
+
+                user = new Student
+                {
+                    FirstName = "Daniel",
+                    LastName = "LaRusso",
+                    Email = "daniel.larusso@lexicon.se",
+                    CourseId = 1,
+                    UserName = "daniel.larusso@lexicon.se"
+
+                };
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Student");
+
+                user = new Student
+                {
+                    FirstName = "Kwai Chang",
+                    LastName = "Caine",
+                    Email = "kwaichang.caine@lexicon.se",
+                    CourseId = 2,
+                    UserName = "kwaichang.caine@lexicon.se"
+
+                };
+
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Student");
+
+                user = new Student
+                {
+                    FirstName = "Forrest",
+                    LastName = "Gump",
+                    Email = "forrest.gump@lexicon.se",
+                    CourseId = 2,
+                    UserName = "forrest.gump@lexicon.se"
+
+                };
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Student");
+
+                user = new Student
+                {
+                    FirstName = "Biff",
+                    LastName = "Henderson",
+                    Email = "biff.henderson@lexicon.se",
+                    CourseId = 3,
+                    UserName = "biff.henderson@lexicon.se"
+
+                };
+
+                result = userManager.Create(user, "VerySecret123!");
+                result = userManager.AddToRole(user.Id, "Student");
+
             }
 
         }
